9bcb4496dd6c66e99d4fca4cbf5958f9faddc5f9
kaylathornton55@icloud.com <kayla_thornton@Kaylas-Air.wifi.gsu.edu>
2025-06-12 12:15:05 -0400

    added catch-all and nonFound error handling for invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,13 +25,18 @@ export async function createInvoice(formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
+  try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (err) {
+    console.error(err)
+  }
     
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -49,18 +54,25 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
  
   const amountInCents = amount * 100;
- 
+  
+  try {
   await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
+  } catch (err) {
+    console.error(err)
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (err) {
+    console.error(err)
+  }
   revalidatePath('/dashboard/invoices');
 }
